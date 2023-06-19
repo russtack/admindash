4b18c38916ca6458bfbb20942cafb6b3bafeb91d
russtack <stirlingest@gmail.com>
2023-06-19 02:37:19 -0400

    5

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./new.scss";
 
 const New = () => {
-  return <div>New</div>;
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>Add New User</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src={
+                file
+                  ? URL.createObjectURL(file)
+                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              }
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form action="">
+              <div className="formInput">
+                <label htmlFor="Username"></label>
+                <input type="text" name="" id="" placeholder="John Doe" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default New;
